5304ac596bb9a5f099a07b5b437ec0fcec9b006b
tanayburbure <tanayburbure@gmail.com>
2026-03-24 21:28:45 +0530

    Added Endpoints

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import express from "express" ; 
+import mongoose from "mongoose" ;
+import jwt from "jsonwebtoken" ;
+
+const app = express() ;
+
+app.post("/api/v1/signup" , (req , res) => {
+
+})
+
+app.post("/api/v1/signup" , (req , res) => {
+
+})
+
+app.get("/api/v1/content" , (req, res) => {
+
+})
+
+app.delete("/api/v1/content" , (req,res) => {
+
+})
+
+app.post("/api/v1/brain/share" , (req, res ) => {
+
+})
+
+app.get("/api/v1/brain/:shareLink" , (req, res) => {
+    
+})
+
+app.listen(8000)
