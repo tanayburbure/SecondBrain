bf14f19ef0b1c908b29a5d302799ab96fecc695d
tanayburbure <tanayburbure@gmail.com>
2026-04-15 23:15:25 +0530

    Added Get , Post , delete content endpoint

--- src/database/db.ts
+++ src/database/db.ts
@@ -19,7 +19,8 @@ const ContentSchema = new Schema({
     title : String ,
     link : String ,
     tag : [{ type : mongoose.Types.ObjectId , ref : 'Tag'}] ,
-    userId : { type : mongoose.Types.ObjectId , ref : 'User' , require : true} 
+    userId : { type : mongoose.Types.ObjectId , ref : 'User' , require : true} ,
+    
 })
 
 export const  ContentModel = mongoose.model("Content" , ContentSchema)
--- src/index.ts
+++ src/index.ts
@@ -57,7 +57,7 @@ app.post("/api/v1/signin" ,async (req , res) => {
     }
 })
 
-app.get("/api/v1/content" ,UserMiddleware , async (req, res) => {
+app.post("/api/v1/content" ,UserMiddleware , async (req, res) => {
     const link = req.body.link ;
     const type = req.body.link ;
 
@@ -76,8 +76,27 @@ app.get("/api/v1/content" ,UserMiddleware , async (req, res) => {
 
 })
 
-app.delete("/api/v1/content" , (req,res) => {
+app.get("/api/v1/content" ,UserMiddleware , async (req, res) => {
+    //@ts-ignore
+    const userId = req,userId ;
+    const content = await ContentModel.find({
+        userId : userId
+    }).populate("userId" , "username")
+    res.json({
+        content
+    })
+})
 
+app.delete("/api/v1/content" ,UserMiddleware, async (req,res) => {
+    const contentId = req.body.contentId ;
+    await ContentModel.deleteMany({
+        contentId,
+        // @ts-ignore
+        userId : req.userId
+    })
+    res.json({
+        message : "Content Deleted"
+    })
 })
 
 app.post("/api/v1/brain/share" , (req, res ) => {
